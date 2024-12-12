e4c8dd45e23197c3f71e6645b97e14d250e12783
Rajkov9 <rajkov.ivan24@gmail.com>
2024-12-12 22:18:40 +0100

    next file

---
+++ Bonus homework JS 1/calculateAge.js
@@ -0,0 +1,8 @@
+while(true){
+    let birthYear = prompt("Please enter your year of birth");
+    const currentYear = new Date().getFullYear();
+    function ageCalculator(birthYear, currentYear){
+        return currentYear - birthYear;
+    }
+    console.log(ageCalculator(birthYear, currentYear))
+}
